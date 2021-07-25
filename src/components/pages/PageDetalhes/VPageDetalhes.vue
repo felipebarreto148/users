7b221e5f133f92b9e31789064c82d7593100d044
felipe barreto <felipebarreto148@gmail.com>
2021-07-25 11:15:01 -0300

    feat: Change code to get data from route params

--- src/components/pages/PageDetalhes/VPageDetalhes.vue
+++ src/components/pages/PageDetalhes/VPageDetalhes.vue
@@ -16,16 +16,9 @@ export default {
   name: "PageDetalhes",
   data() {
     return {
-      nome: "teste",
-      idade: 190,
-      sobre: `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in sem
-        laoreet turpis iaculis aliquet non nec ex. Quisque hendrerit molestie
-        massa, sed eleifend sapien. Aliquam dapibus ornare leo, vitae fermentum
-        lectus rutrum ac. Donec non felis eu nunc dignissim ullamcorper. Nunc
-        non diam eu urna posuere lacinia. Nunc turpis tortor, sollicitudin ac
-        pretium eu, fermentum sit amet nisi. Integer metus felis, posuere nec
-        velit vitae, facilisis blandit libero.`,
+      nome: this.$route.params.user.nome || "",
+      idade:this.$route.params.user.idade || "",
+      sobre: this.$route.params.user.sobre || "",
     };
   },
 };
